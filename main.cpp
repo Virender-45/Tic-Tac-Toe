07ff779959c7d29063befd3c8cae9573a48994bf
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-03-08 10:58:32 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWInner(char *spaces, char player, char compuetr);
+bool checkTie(char *spaces);
+
+int main()
+{
+    char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+    return 0;
+}
